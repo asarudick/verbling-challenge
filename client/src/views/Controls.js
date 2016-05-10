ed2afa7e2f06ccbb9569f88338235b81497df47a
Shabonkerz <Shabonkerz@gmail.com>
2016-05-10 08:21:55 -0500

    modify css/html to be more semantic

--- client/src/views/Controls.js
+++ client/src/views/Controls.js
@@ -3,7 +3,16 @@ import React from 'react';
 export default class Controls extends React.Component {
 
     render() {
-        return <div className='controls'></div>;
+        return 	<div className='controls'>
+					<div className="left">
+						<button className='open-all'>Open All</button>
+						<button className='close-all'>Close All</button>
+						<button className='toggle-all'>Toggle All</button>
+					</div>
+					<div className="right">
+						<button className='add'>Add</button>
+					</div>
+				</div>;
     }
 
 }
