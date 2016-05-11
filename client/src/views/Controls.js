620f7a89e053beeaabdfc9d16d74862734d1aa35
Shabonkerz <Shabonkerz@gmail.com>
2016-05-11 01:52:59 -0500

    wire up controls to modify model

--- client/src/views/Controls.js
+++ client/src/views/Controls.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
 export default class Controls extends React.Component {
+	onExpandAll () {
+		this.props.items.expandAll();
+	}
+
+	onCollapseAll () {
+		this.props.items.collapseAll();
+	}
+
+	onToggleAll () {
+		this.props.items.toggleAll();
+	}
+
+	onAdd () {
+		const text = window.prompt('What would you like to add?');
+		this.props.items.add(text);
+	}
 
     render() {
         return 	<div className='controls'>
 					<div className="left">
-						<button className='open-all'>Open All</button>
-						<button className='close-all'>Close All</button>
-						<button className='toggle-all'>Toggle All</button>
+						<button onClick={this.onExpandAll.bind(this)}>Expand All</button>
+						<button onClick={this.onCollapseAll.bind(this)}>Collapse All</button>
+						<button onClick={this.onToggleAll.bind(this)}>Toggle All</button>
 					</div>
 					<div className="right">
-						<button className='add'>Add</button>
+						<button onClick={this.onAdd.bind(this)}>Add</button>
 					</div>
 				</div>;
     }
